af75e5c74536110e5c6941ed86c38d6c0e1f56a9
VieronikaHontar <ronnicka@gmail.com>
2019-04-06 10:42:08 +0300

    Updates

---
+++ precache-manifest.6dddd11e2d964a4f5eb3392473a57da6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fa9dfa60ed02433a308",
+    "url": "/exel-react/static/js/runtime~main.9fa9dfa6.js"
+  },
+  {
+    "revision": "fdf63d466939a0a11bc1",
+    "url": "/exel-react/static/js/main.fdf63d46.chunk.js"
+  },
+  {
+    "revision": "1cc0371338f0907d9429",
+    "url": "/exel-react/static/js/1.1cc03713.chunk.js"
+  },
+  {
+    "revision": "fdf63d466939a0a11bc1",
+    "url": "/exel-react/static/css/main.a7160305.chunk.css"
+  },
+  {
+    "revision": "1cc0371338f0907d9429",
+    "url": "/exel-react/static/css/1.aa7f9768.chunk.css"
+  },
+  {
+    "revision": "61d3420ee63248a096a7caa47542877b",
+    "url": "/exel-react/index.html"
+  }
+];
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import cell from "./cell";
-import table from "./table";
-
-export default combineReducers({
-	cell,
-	table
-});
